cb214b1774f15beb602db3286e24f2a6d5e742bc
Valentin Kuznetsov <aimestereo@gmail.com>
2020-12-18 13:37:44 +0300

    Add utils method to get any admin action method

--- admin_actions/utils.py
+++ admin_actions/utils.py
@@ -1,11 +1,17 @@
-from typing import Type
+from typing import Type, TYPE_CHECKING
 
 from django.shortcuts import redirect
 from django.urls import reverse
 
 
+if TYPE_CHECKING:
+    from django.contrib.admin import ModelAdmin  # noqa
+    from django.db import models  # noqa
+
+
 __all__ = [
     "redirect_to_list_view",
+    "get_admin_url",
     "get_object_url",
     "get_object_url_from_obj",
 ]
@@ -17,10 +23,14 @@ def redirect_to_list_view(admin: "ModelAdmin"):
     return redirect(f"admin:{urlpattern.name}")
 
 
-def get_object_url(model: Type["models.Model"], pk: int):
+def get_admin_url(model: Type["models.Model"], method_name="change", args=None, kwargs=None):
     opts = model._meta
-    return reverse(f"admin:{opts.app_label}_{opts.model_name}_change", args=(pk,))
+    return reverse(f"admin:{opts.app_label}_{opts.model_name}_{method_name}", args=args, kwargs=kwargs)
+
+
+def get_object_url(model: Type["models.Model"], pk: int):
+    return get_admin_url(model, method_name="change", args=(pk,))
 
 
 def get_object_url_from_obj(obj: "models.Model"):
-    return get_object_url(obj.__class__, obj.pk)
+    return get_admin_url(obj.__class__, method_name="change", args=(obj.pk,))
